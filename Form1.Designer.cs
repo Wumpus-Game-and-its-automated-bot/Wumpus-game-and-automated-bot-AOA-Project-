d9b70deaf33ca2110c84701053e4f41d909099d1
Faheem955 <57995622+Faheem955@users.noreply.github.com>
2019-11-30 23:56:09 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Wumpus_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pb12 = new System.Windows.Forms.PictureBox();
+            this.pb13 = new System.Windows.Forms.PictureBox();
+            this.pb14 = new System.Windows.Forms.PictureBox();
+            this.pb15 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb10 = new System.Windows.Forms.PictureBox();
+            this.pb11 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb0 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb0)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pb0);
+            this.groupBox1.Controls.Add(this.pb1);
+            this.groupBox1.Controls.Add(this.pb2);
+            this.groupBox1.Controls.Add(this.pb3);
+            this.groupBox1.Controls.Add(this.pb4);
+            this.groupBox1.Controls.Add(this.pb7);
+            this.groupBox1.Controls.Add(this.pb6);
+            this.groupBox1.Controls.Add(this.pb5);
+            this.groupBox1.Controls.Add(this.pb9);
+            this.groupBox1.Controls.Add(this.pb10);
+            this.groupBox1.Controls.Add(this.pb11);
+            this.groupBox1.Controls.Add(this.pb8);
+            this.groupBox1.Controls.Add(this.pb15);
+            this.groupBox1.Controls.Add(this.pb14);
+            this.groupBox1.Controls.Add(this.pb13);
+            this.groupBox1.Controls.Add(this.pb12);
+            this.groupBox1.Location = new System.Drawing.Point(32, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(551, 605);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Wumpus Bot";
+            // 
+            // pb12
+            // 
+            this.pb12.Location = new System.Drawing.Point(6, 458);
+            this.pb12.Name = "pb12";
+            this.pb12.Size = new System.Drawing.Size(130, 130);
+            this.pb12.TabIndex = 0;
+            this.pb12.TabStop = false;
+            // 
+            // pb13
+            // 
+            this.pb13.Location = new System.Drawing.Point(142, 458);
+            this.pb13.Name = "pb13";
+            this.pb13.Size = new System.Drawing.Size(130, 130);
+            this.pb13.TabIndex = 1;
+            this.pb13.TabStop = false;
+            // 
+            // pb14
+            // 
+            this.pb14.Location = new System.Drawing.Point(278, 458);
+            this.pb14.Name = "pb14";
+            this.pb14.Size = new System.Drawing.Size(130, 130);
+            this.pb14.TabIndex = 2;
+            this.pb14.TabStop = false;
+            // 
+            // pb15
+            // 
+            this.pb15.Location = new System.Drawing.Point(414, 458);
+            this.pb15.Name = "pb15";
+            this.pb15.Size = new System.Drawing.Size(130, 130);
+            this.pb15.TabIndex = 3;
+            this.pb15.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.Location = new System.Drawing.Point(142, 322);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(130, 130);
+            this.pb9.TabIndex = 1;
+            this.pb9.TabStop = false;
+            // 
+            // pb10
+            // 
+            this.pb10.Location = new System.Drawing.Point(278, 322);
+            this.pb10.Name = "pb10";
+            this.pb10.Size = new System.Drawing.Size(130, 130);
+            this.pb10.TabIndex = 2;
+            this.pb10.TabStop = false;
+            // 
+            // pb11
+            // 
+            this.pb11.Location = new System.Drawing.Point(415, 322);
+            this.pb11.Name = "pb11";
+            this.pb11.Size = new System.Drawing.Size(130, 130);
+            this.pb11.TabIndex = 3;
+            this.pb11.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(6, 322);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(130, 130);
+            this.pb8.TabIndex = 4;
+            this.pb8.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(143, 186);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(130, 130);
+            this.pb5.TabIndex = 5;
+            this.pb5.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(279, 186);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(130, 130);
+            this.pb6.TabIndex = 6;
+            this.pb6.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(415, 186);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(130, 130);
+            this.pb7.TabIndex = 7;
+            this.pb7.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(6, 186);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(130, 130);
+            this.pb4.TabIndex = 8;
+            this.pb4.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(414, 50);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(130, 130);
+            this.pb3.TabIndex = 9;
+            this.pb3.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(278, 50);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(130, 130);
+            this.pb2.TabIndex = 10;
+            this.pb2.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(143, 50);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(130, 130);
+            this.pb1.TabIndex = 11;
+            this.pb1.TabStop = false;
+            // 
+            // pb0
+            // 
+            this.pb0.Location = new System.Drawing.Point(7, 50);
+            this.pb0.Name = "pb0";
+            this.pb0.Size = new System.Drawing.Size(130, 130);
+            this.pb0.TabIndex = 12;
+            this.pb0.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 634);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb0)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pb0;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb10;
+        private System.Windows.Forms.PictureBox pb11;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb15;
+        private System.Windows.Forms.PictureBox pb14;
+        private System.Windows.Forms.PictureBox pb13;
+        private System.Windows.Forms.PictureBox pb12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wumpus_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pb12.Image = Properties.Resources.agent;
+            Random rnd = new Random();
+            int pitrnd = rnd.Next(0, 16);
+            int wumpusrnd;
+            int goldrnd;
+            printPit(pitrnd);
+            do
+            {
+                wumpusrnd = rnd.Next(0, 16);
+            }
+            while (wumpusrnd == pitrnd);
+            printWumpus(wumpusrnd);
+            do
+            {
+                goldrnd = rnd.Next(0, 16);
+            }
+            while (goldrnd == pitrnd || goldrnd == wumpusrnd);
+            printGold(goldrnd);
+        }
+
+
+        void printPit(int num)
+        {
+            switch (num)
+            {
+                case 0:
+                    pb0.Image = Properties.Resources.pit;
+                    break;
+                case 1:
+                    pb1.Image = Properties.Resources.pit;
+                    break;
+                case 2:
+                    pb2.Image = Properties.Resources.pit;
+                    break;
+                case 3:
+                    pb3.Image = Properties.Resources.pit;
+                    break;
+                case 4:
+                    pb4.Image = Properties.Resources.pit;
+                    break;
+                case 5:
+                    pb5.Image = Properties.Resources.pit;
+                    break;
+                case 6:
+                    pb6.Image = Properties.Resources.pit;
+                    break;
+                case 7:
+                    pb7.Image = Properties.Resources.pit;
+                    break;
+                case 8:
+                    pb8.Image = Properties.Resources.pit;
+                    break;
+                case 9:
+                    pb9.Image = Properties.Resources.pit;
+                    break;
+                case 10:
+                    pb13.Image = Properties.Resources.pit;
+                    break;
+                case 11:
+                    pb9.Image = Properties.Resources.pit;
+                    break;
+                case 13:
+                    pb1.Image = Properties.Resources.pit;
+                    break;
+                case 14:
+                    pb14.Image = Properties.Resources.pit;
+                    break;
+                case 15:
+                    pb15.Image = Properties.Resources.pit;
+                    break;
+                default:
+                    pb10.Image = Properties.Resources.pit;
+                    break;
+            }
+
+        }
+
+        void printGold(int num)
+        {
+            switch (num)
+            {
+                case 0:
+                    pb0.Image = Properties.Resources.gold;
+                    break;
+                case 1:
+                    pb1.Image = Properties.Resources.gold;
+                    break;
+                case 2:
+                    pb2.Image = Properties.Resources.gold;
+                    break;
+                case 3:
+                    pb3.Image = Properties.Resources.gold;
+                    break;
+                case 4:
+                    pb4.Image = Properties.Resources.gold;
+                    break;
+                case 5:
+                    pb5.Image = Properties.Resources.gold;
+                    break;
+                case 6:
+                    pb6.Image = Properties.Resources.gold;
+                    break;
+                case 7:
+                    pb7.Image = Properties.Resources.gold;
+                    break;
+                case 8:
+                    pb8.Image = Properties.Resources.gold;
+                    break;
+                case 9:
+                    pb9.Image = Properties.Resources.gold;
+                    break;
+                case 10:
+                    pb13.Image = Properties.Resources.gold;
+                    break;
+                case 11:
+                    pb9.Image = Properties.Resources.gold;
+                    break;
+                case 13:
+                    pb1.Image = Properties.Resources.gold;
+                    break;
+                case 14:
+                    pb14.Image = Properties.Resources.gold;
+                    break;
+                case 15:
+                    pb15.Image = Properties.Resources.gold;
+                    break;
+                default:
+                    pb2.Image = Properties.Resources.gold;
+                    break;
+            }
+
+        }
+
+        void printWumpus(int num)
+        {
+            switch (num)
+            {
+                case 0:
+                    pb0.Image = Properties.Resources.wumpus;
+                    break;
+                case 1:
+                    pb1.Image = Properties.Resources.wumpus;
+                    break;
+                case 2:
+                    pb2.Image = Properties.Resources.wumpus;
+                    break;
+                case 3:
+                    pb3.Image = Properties.Resources.wumpus;
+                    break;
+                case 4:
+                    pb4.Image = Properties.Resources.wumpus;
+                    break;
+                case 5:
+                    pb5.Image = Properties.Resources.wumpus;
+                    break;
+                case 6:
+                    pb6.Image = Properties.Resources.wumpus;
+                    break;
+                case 7:
+                    pb7.Image = Properties.Resources.wumpus;
+                    break;
+                case 8:
+                    pb8.Image = Properties.Resources.wumpus;
+                    break;
+                case 9:
+                    pb9.Image = Properties.Resources.wumpus;
+                    break;
+                case 10:
+                    pb13.Image = Properties.Resources.wumpus;
+                    break;
+                case 11:
+                    pb9.Image = Properties.Resources.wumpus;
+                    break;
+                case 13:
+                    pb1.Image = Properties.Resources.wumpus;
+                    break;
+                case 14:
+                    pb14.Image = Properties.Resources.wumpus;
+                    break;
+                case 15:
+                    pb15.Image = Properties.Resources.wumpus;
+                    break;
+                default:
+                    pb6.Image = Properties.Resources.wumpus;
+                    break;
+            }
+
+        }
+    }
+}
